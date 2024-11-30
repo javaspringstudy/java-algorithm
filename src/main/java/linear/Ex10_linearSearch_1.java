4d80e39d2a8995b9feb391ee9f01fc569ad5eacc
javaspringstudy <java.spring.study@gmail.com>
2024-11-30 17:36:41 +0900

    이진검색-1

--- src/main/java/linear/Ex10_linearSearch_1.java
+++ src/main/java/linear/Ex10_linearSearch_1.java
@@ -1,7 +1,8 @@
 package linear;
 
-//선형검색, 순차검색
+//선형검색, 순차검색 - 시간복잡도 O(n)
 //정렬되지 않은 배열에서 검색할 때 사용하는 유일한 방법
+//데이터가 작고 간단할 때 유용하며, 빠르게 구현할 수 있는 장점
 //배열에서 값이 '둘리'인 원소의 인덱스 추출
 public class Ex10_linearSearch_1 {
     public int srch(String[] friends, String name) {
--- src/main/java/linear/Ex10_linearSearch_2.java
+++ src/main/java/linear/Ex10_linearSearch_2.java
@@ -1,16 +1,16 @@
 package linear;
 
+//선형검색, 순차검색 - 시간복잡도 O(n)
+//정렬되지 않은 배열에서 검색할 때 사용하는 유일한 방법
+//데이터가 작고 간단할 때 유용하며, 빠르게 구현할 수 있는 장점
 public class Ex10_linearSearch_2 {
-
-    //선형검색, 순차검색
-    //정렬되지 않은 배열에서 검색할 때 사용하는 유일한 방법
     public int srch(int[] nums, int targetNum) {
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==targetNum) return 1;////종료조건2 : 검색성공 1
+            if(nums[i]==targetNum) return 1;//종료조건1 : 검색성공 1
         }
 
-        return -1;//종료조건1 : 검색실패 -1
+        return -1;//종료조건2 : 검색실패 -1
 
     }
 }
